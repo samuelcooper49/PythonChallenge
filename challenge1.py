f1ccafeecb3bcd4da0704bb1adffbc0e781d4bfe
John Edwards <john.edwards@linguamatics.com>
2017-07-03 14:44:55 +0100

    Refactored challenge1 into functions to make it testable. Removed the use of global variables to map characters and opted for a dictionary instead. Added unit tests.

--- challenge1.py
+++ challenge1.py
@@ -1,37 +1,55 @@
-a = "C"
-b = "D"
-c = "E"
-d = "F"
-e = "G"
-f = "H"
-g = "I"
-h = "J"
-i = "K"
-j = "L"
-k = "M"
-l = "N"
-m = "O"
-n = "P"
-o = "Q"
-p = "R"
-q = "S"
-r = "T"
-s = "U"
-t = "V"
-u = "W"
-v = "X"
-w = "Y"
-x = "Z"
-y = "A"
-z = "B"
+#!/usr/bin/env python3
 
-#text = """g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q
-#ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."""
-text2 = """map"""
+# Solution to Python Challenge Part 1: http://www.pythonchallenge.com/pc/def/map.html
 
-for letter in text2:
-    if letter in globals():
-        print(globals()[letter], end='')
-    else:
-        print(letter, end='')
+MAP={
+    "a" : "c",
+    "b" : "d",
+    "c" : "e",
+    "d" : "f",
+    "e" : "g",
+    "f" : "h",
+    "g" : "i",
+    "h" : "j",
+    "i" : "k",
+    "j" : "l",
+    "k" : "m",
+    "l" : "n",
+    "m" : "o",
+    "n" : "p",
+    "o" : "q",
+    "p" : "r",
+    "q" : "s",
+    "r" : "t",
+    "s" : "u",
+    "t" : "v",
+    "u" : "w",
+    "v" : "x",
+    "w" : "y",
+    "x" : "z",
+    "y" : "a",
+    "z" : "b",
+}
 
+def decipher(cipher_text):
+    """
+    decipher input cipher_text using a simple letter exhange mapping
+    inputs:
+       cipher_text : string, ciphered text
+    Outputs:
+       str : plain text
+    Failure:
+       Exception
+    """
+    plain_text = ""
+    for letter in cipher_text:
+        if letter in MAP:
+            plain_text += MAP[letter]
+        else:
+            plain_text += letter
+    return(plain_text)
+
+if __name__ == "__main__":
+    text = """g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q
+ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."""
+    print(decipher(text))
--- tests.py
+++ tests.py
@@ -2,11 +2,19 @@
 
 import unittest
 import challenge0
+import challenge1
 
 class tests_challenge0(unittest.TestCase):
     """Tests for challenge0"""
     def test_exponentiate(self):
         self.assertEqual(challenge0.exponentiate(2,38), int(274877906944))
+
+class tests_challenge1(unittest.TestCase):
+    """Tests for challenge1"""
+    def test_exponentiate(self):
+        self.assertEqual(challenge1.decipher("""g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q
+ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."""), str("""i hope you didnt translate it by hand. thats what computers are for. doing it in by hand is inefficient and that's
+why this text is so long. using string.maketrans() is recommended. now apply on the url."""))
 
 if __name__ == "__main__":
     unittest.main()
